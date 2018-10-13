c1810b95bfea4cced1d2dcf208d6671670292948
NiRuaG <gaurin1107@gmail.com>
2018-10-13 00:43:46 -0500

    redo p1 p2 connections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,15 +7,25 @@ $(document).ready(function() {
     input_submit: null,
      form_alert : null,
 
-    playerName  : null,
-    playerChoice: null,
-       oppName  : null,
-       oppChoice: null
+     players : null,
   };
   for (let id of Object.keys(JQ_IDs)) {
     JQ_IDs[id] = $(`#${id}`);
   }
 
+  let JQ_CLASSes = {
+    playerDiv : null,
+
+    pType  : null,
+    pName  : null,
+    pChoice: null,
+  }
+  for (let cl of Object.keys(JQ_CLASSes)) {
+    JQ_CLASSes[cl] = $(`.${cl}`);
+  }
+
+  let playersJoined = 0;
+  let playersJoined_me = -1;
   // #region Firebase Setup  
   let db = loadPersonalFirebase();
   // let db = {};
@@ -31,36 +41,50 @@ $(document).ready(function() {
   // User Entered Name to Connect to Game
   JQ_IDs.input_submit.click(function(event){
     event.preventDefault();
+    JQ_IDs.form_alert.empty();
+    
+    if (playersJoined === 2) {
+      JQ_IDs.form_alert.html("Sorry. Game is full. Please wait.");
+      return;
+    }
+
     let name = JQ_IDs.input_name.val().trim();
     JQ_IDs.input_name.val(""); // empty-out input field
     if (!name){ return; } // ignore empty input
-    if (playersRef_me) { return; } // ignore trying to connect again
+    if (playersRef_me) { return; } // ignore trying to make another connection
 
-    playersRef_me = playersRef.child(name);
-    playersRef_me.onDisconnect().remove();
-    playersRef_me.once("value", function(snap) {
-      if (snap.exists()) {
-          JQ_IDs.form_alert.html(`A Player with name <strong>${name}</strong> is already in the game.`);
-          playersRef_me = null;
-      }
-      else {
-        playersRef_me.set({
-          name: name,
-        });
-        JQ_IDs.playerName.text(name);
-        JQ_IDs.input_form.hide();
-      }
+    let ref = playersRef.child(playersJoined);
+    ref.onDisconnect().remove();
+    playersJoined_me = playersJoined;
+    ref.set({
+      name: name,
     });
+    JQ_IDs.input_form.hide();
+    // playersRef_me = playersRef.child(name);
+    // playersRef_me.onDisconnect().remove();
+    // playersRef_me.once("value", function(snap) {
+    //   if (snap.exists()) {
+    //       JQ_IDs.form_alert.html(`A Player with name <strong>${name}</strong> is already in the game.`);
+    //       playersRef_me = null;
+    //   }
+    //   else {
+    //     playersRef_me.set({
+    //       name: name,
+    //     });
+    //     JQ_IDs.playerName.text(name);
+    //     JQ_IDs.input_form.hide();
+    //   }
+    // });
   });
   // #endregion Clicks
 
 
   // #region Firebase Event Handlers
-  connectedRef.on("value", function(snap) {
-    console.log(snap);
+  connectedRef.on("value", function(dataSnap) {
+    let val = dataSnap.val();
+    console.log("connected/value", val);
     
-    console.log("connection value change", snap.val());
-    if (snap.val()){
+    if (val) {
       let con = connectionsRef.push();
       con.onDisconnect().remove();
       con.set(true);
@@ -72,15 +96,39 @@ $(document).ready(function() {
   //   JQ_IDs.playerName.text(name);
   // });
   
-  playersRef.on("child_added", function(childSnap, prevChildKey) {
-    console.log("player/child_add", childSnap, childSnap.val(), prevChildKey || null);
-    // See if this is my opponet joining the game 
-    if (  (!prevChildKey && !playersRef_me ) //  first connection and I haven't joined yet 
-       || ( prevChildKey && !playersRef_opp) // second connection and opponent hasn't already
-        ) {
-      JQ_IDs.oppName.text(childSnap.val().name);
-      playersRef_opp = playersRef.child(childSnap.val().name);
-    }
+  // playersRef.on("child_added", function(childSnap, prevChildKey) {
+  //   ++playersJoined;
+  //   let val = childSnap.val();
+  //   console.log("player/child_add", val, prevChildKey || null, playersJoined);
+  //   // See if this is my opponet joining the game 
+  //   if (  (!prevChildKey && !playersRef_me ) //     first connection and    I    haven't joined yet 
+  //      || ( prevChildKey && !playersRef_opp) // not-first connection and opponent hadn't joined yet
+  //       ) {
+  //     JQ_IDs.oppName.text(val.name);
+  //     playersRef_opp = playersRef.child(val.name);
+  //   }
+  // });
+
+  // playersRef.on("child_removed", function(oldChildSnap) {
+  //   --playersJoined;
+  //   let val = oldChildSnap.val();
+  //   console.log("player/child_removed", val, playersJoined);
+  //   if (playersRef_opp.key === val.name) {
+  //     console.log("My opponent left");
+  //   }
+  // });
+  
+  playersRef.on("value", function(dataSnap) {
+    let val = dataSnap.val();
+    console.log("players/value", val || null);
+    dataSnap.forEach(function(childSnap) {
+      // console.log(childSnap.key, childSnap.val());
+      // console.log(JQ_CLASSes.pName[childSnap.key]);
+      console.log(childSnap.key);
+      JQ_CLASSes.pType.eq(childSnap.key).text(+childSnap.key === playersJoined_me ? '(ME)' : '(Opponent)');
+      JQ_CLASSes.pName.eq(childSnap.key).text(childSnap.val().name);
+    });
+    playersJoined = dataSnap.numChildren();
   });
   // #endregion Firebase Event Handlers
 });
